2cdde3a58696f4dd09cdd3919ca103dfd6e9d74c
charles13 <charles13@users.noreply.github.com>
2015-01-30 08:00:42 -0600

    Create SPItest.py

---
+++ Python-Examples/SPItest.py
@@ -0,0 +1,30 @@
+"""
+SPItest.py
+This example test the functionality of SPI to check the writing process
+Using the Arduino Expansion Board:
+SCLK = Pin 13
+MISO = Pin 12
+MOSI = Pin 11
+EN = Pin 10
+
+This example code is in the public domain.
+
+Revision History
+------------------------------------------------
+Author			  Date			  Description
+------------------------------------------------
+Carlos Mata			1-29-2015		Example created
+"""
+
+import time
+import mraa
+
+spi= mraa.Spi(0)
+buff = bytearray('\x02\xee\x10')
+try:
+        while True:
+                spi.writeByte(0x12)
+                spi.writeByte(0x35)
+                spi.write(buff)
+except KeyboardInterrupt:
+        print ""
