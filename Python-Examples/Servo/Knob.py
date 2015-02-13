1c67d809bf7c4c3f236b95adada596b9d10488cf
D-Villa <d.villalobos8@gmail.com>
2015-02-13 06:28:55 -0600

    Create Knob.py

---
+++ Python-Examples/Servo/Knob.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+"""
+Knob.py
+
+This example shows how to use a servo motor attached
+to a PWM pin through a potenciometer.
+
+This example code is in the public domain.
+
+Revision History
+----------------------------------------------------------
+    Author		   Date		  Description
+----------------------------------------------------------
+Diego Villalobos	02-12-2015	Example created
+
+"""
+
+# Libraries required
+from Servo import *
+import mraa
+import time
+
+# Create a new servo object with a reference name
+myServo = Servo("First Servo")
+
+# Attaches the servo to pin 3 in Arduino Expansion board
+myServo.attach(3)
+
+# Attaches the potenciometer to analog input A0
+# in Arduino Expansion Board
+pot = mraa.Aio(0)
+pot.setBit(12)
+valuePot = 0
+
+# Print servo settings
+print ""
+print "*** Servo Initial Settings ***"
+print myServo
+print ""
+
+print "Move the knob."
+print ""
+
+try:
+    # Move the servo using the potenciometer
+    while True:
+        # Read the analog value from pot
+        valuePot = pot.read()
+
+        # mapValue() is in the Servo library
+        angle = mapValue(valuePot, 0, 4096, 0, 180)
+
+        # Write the angle
+        myServo.write(angle)
+        time.sleep(0.005)
+            
+except KeyboardInterrupt:
+        print "Knob ended."
