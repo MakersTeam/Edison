1fe242ad94c61a8f004afd8e9bb51108901ade73
D-Villa <d.villalobos8@gmail.com>
2015-09-14 08:46:53 -0600

    Create retrieveWiFiPassword.ino

---
+++ Arduino-Examples/retrieveWiFiPassword.ino
@@ -0,0 +1,28 @@
+/*
+retrieveWiFiPassword.ino - Shows the WiFi AP password for 3 seconds
+
+This example shows the WiFi AP password for 3 seconds in case it has been forgotten.
+This example code was originally written by the user Intel_Raad and shared it in the 
+Edison's Community: https://communities.intel.com/thread/60076
+
+This example only works on Edison.
+
+This example code is in the public domain.
+
+Revision History
+
+----------------------------------------------------------
+    Author		   Date		  Description
+----------------------------------------------------------
+Diego Villalobos	09-14-2015	Example created
+
+*/
+
+void setup() {
+     delay(3000);
+     system("grep '^wpa_passphrase' /etc/hostapd/hostapd.conf | sed 's/=/ /g' | awk '{print $2}' > /dev/ttyGS0");
+}
+ 
+void loop() {
+  
+}
